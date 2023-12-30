b5d873037cd5e46bdafa7b1e1019746583609d69
kubazap <jz.jakub.zapala@gmail.com>
2023-12-30 17:20:43 +0100

    Refactoring EkranPowitalny.js

--- screens/EkranPowitalny.js
+++ screens/EkranPowitalny.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Image } from "react-native";
-import { StyleSheet, View, Text, Pressable } from "react-native";
+import { StyleSheet, View, Text, Pressable, Dimensions } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { FontSize, FontFamily, Color, Border } from "../GlobalStyles";
 
@@ -12,7 +12,7 @@ const EkranPowitalny = () => {
       <View style={styles.frame1}>
         <Image
           style={styles.frameIcon}
-          contentFit="cover"
+          resizeMode="contain"
           source={require("../assets/frame.png")}
         />
         <Image
@@ -34,115 +34,136 @@ const EkranPowitalny = () => {
           style={styles.przycisk}
           onPress={() => navigation.navigate("Logowanie")}
         >
-          <View style={styles.rectangle} />
-          <Text style={styles.zalogujSi}>Zaloguj się</Text>
+          <View style={styles.rectangle}>
+            <Text style={styles.zalogujSi}>Zaloguj się</Text>
+          </View>
         </Pressable>
+        
+      </View>
+      <View style={styles.frame4}>
+        <Image
+          style={styles.frameIcon2}
+          contentFit="cover"
+          source={require("../assets/Maska_z_cieniem.png")}
+        />
+        <View style={styles.frame}>
+        </View>
       </View>
     </View>
   );
 };
 
+const { width, height } = Dimensions.get("window");
+
 const styles = StyleSheet.create({
   frameIcon: {
-    position: "absolute",
-    top: 0,
-    left: 225,
-    width: 304,
-    height: 370,
-    overflow: "hidden",
+    width: width, 
+    height: height * 0.5,  
+    alignSelf: "center", 
+    zIndex: 2, 
+    overflow: "visible", 
+    marginTop: height * 0.1,
   },
+
   frameIcon1: {
-    position: "absolute",
-    top: 38,
-    left: 171,
-    width: 702,
-    height: 525,
-    overflow: "hidden",
+    position: 'absolute',
+    top: "0%",
+    width: width,
+    height: height,
+    alignItems: "center",
+    zIndex: 1,
   },
+
+  frameIcon2: {
+    position: 'absolute',
+    bottom: "0%",
+    width: width,
+    height: height,
+    alignItems: "center",
+  },
+
   frame: {
-    position: "absolute",
-    top: 386,
-    left: 139,
-    width: 424,
+    width: width,
+    height: height * 0.5,
     overflow: "hidden",
-    flexDirection: "column",
-    alignItems: "flex-start",
+    alignItems: "center",
     justifyContent: "center",
   },
+
   frame1: {
-    position: "absolute",
-    top: 82,
-    left: -171,
-    width: 702,
-    height: 698,
-    overflow: "hidden",
+    width: width,
+    height: height * 0.6,
+    //overflow: "hidden",
   },
+
+  frame4: {
+    width: width,
+    height: height * 0.6,
+    //overflow: "hidden",
+  },
+
   galacticAtlas: {
-    position: "relative",
     fontSize: FontSize.size_17xl,
     fontWeight: "800",
     fontFamily: FontFamily.montserratExtraBold,
     color: Color.text,
     textAlign: "center",
-    width: 389,
-    height: 73,
+    width: width,
+    marginTop: "10%",
   },
+
   odkryjZNami: {
-    position: "relative",
     fontSize: FontSize.size_sm,
     fontFamily: FontFamily.montserratRegular,
     color: Color.secondary,
     textAlign: "center",
-    width: 389,
-    height: 37,
-    marginTop: -22,
+    width: width,
+    marginTop: 5,
   },
+
   napisy: {
-    width: 389,
-    height: 88,
-    flexDirection: "column",
+    width: width,
+    marginTop: "8%",
     alignItems: "center",
-    justifyContent: "flex-start",
-  },
-  frame2: {
-    width: 390,
-    overflow: "hidden",
-    flexDirection: "column",
-    alignItems: "flex-start",
     justifyContent: "center",
   },
+
+  frame2: {
+    width: width,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   rectangle: {
-    position: "absolute",
     height: "100%",
     width: "100%",
-    top: "0%",
-    right: "0%",
-    bottom: "0%",
-    left: "0%",
     borderRadius: Border.br_base,
     backgroundColor: Color.secondary,
     shadowColor: "rgba(255, 203, 116, 0.2)",
     shadowOffset: {
       width: 0,
-      height: 20,
+      height: 10,
     },
-    shadowRadius: 60,
-    elevation: 60,
+    shadowRadius: 30,
+    elevation: 30,
     shadowOpacity: 1,
+    padding: 16,
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    zIndex: 1,
   },
+
   zalogujSi: {
-    position: "absolute",
-    top: "29.63%",
-    left: "23.89%",
     fontSize: FontSize.size_base,
     letterSpacing: 0.4,
     fontWeight: "600",
     fontFamily: FontFamily.montserratSemiBold,
     color: Color.colorGray_100,
     textAlign: "center",
+    zIndex: 1,
   },
+
   przycisk: {
-    position: "relative",
     shadowColor: "rgba(0, 0, 0, 0.25)",
     shadowOffset: {
       width: 0,
@@ -151,29 +172,28 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 4,
     shadowOpacity: 1,
-    width: 180,
+    width: width * 0.5,
     height: 54,
-    marginTop: 60,
+    marginTop: "15%",
+    zIndex: 1,
   },
+
   frame3: {
-    position: "absolute",
-    top: 512,
-    left: 15,
-    width: 390,
-    height: 202,
+    width: width,
+    height: height * 0.3,
     overflow: "hidden",
-    flexDirection: "column",
     alignItems: "center",
-    justifyContent: "flex-start",
+    justifyContent: "flex-end",
+    zIndex: 1,
   },
+
   ekranPowitalny: {
-    position: "relative",
     borderRadius: Border.br_21xl,
     backgroundColor: Color.colorGray_500,
     flex: 1,
-    width: "100%",
-    height: 780,
+    width: width,
     overflow: "hidden",
+    zIndex: 0,
   },
 });
 
